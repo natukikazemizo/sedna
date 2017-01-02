209fa24e72c04d9ede63ba415387e2a20414ed68
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-01-02 16:30:16 +0900

    カスタムGUIの追加
    flip_next_paste_pose.pyをGUIから呼び出せるようにしました。

--- Sedna/src/python/flip_next_paste_pose.py
+++ Sedna/src/python/flip_next_paste_pose.py
@@ -14,24 +14,25 @@ def copy_pose(srcFrame, destFrame, flip):
     bpy.ops.anim.keyframe_insert_menu(\
         type='__ACTIVE__', confirm_success=True)
 
-# Settings
-START_FRAME = 1
-END_FRAME = 4
-# ENABLE_KEY_LIST = [1, 2, 3, 4, 5, 6, 7, 8]
-STEP = 1
+def flip_next_copy(startFrame, endFrame, step):
+    # Settings
+    START_FRAME = startFrame
+    END_FRAME = endFrame
+    # ENABLE_KEY_LIST = [1, 2, 3, 4, 5, 6, 7, 8]
+    STEP = step
+    print("START:%s END:%s STEP:%s" %(START_FRAME, END_FRAME, STEP))
+    print("### COPY START ###")
 
-print("### START ###")
+    i = START_FRAME
+    offset = END_FRAME - START_FRAME + 1
 
-i = START_FRAME
-offset = END_FRAME - START_FRAME + 1
+    bpy.ops.pose.select_all(action='SELECT')
 
-bpy.ops.pose.select_all(action='SELECT')
+    for i in range(START_FRAME, END_FRAME + 1):
+        #if i in ENABLE_KEY_LIST:
+        if i % STEP == 1 or STEP == 1:
+            copy_pose(i, i + offset, True)
+            print("i:" + str(i) )
+    copy_pose(START_FRAME, START_FRAME + offset * 2, False)
 
-for i in range(START_FRAME, END_FRAME + 1):
-    #if i in ENABLE_KEY_LIST:
-    if i % STEP == 1 or STEP == 1:
-        copy_pose(i, i + offset, True)
-        print("i:" + str(i) )
-copy_pose(START_FRAME, START_FRAME + offset * 2, False)
-
-print("### END ###")
+    print("### COPY END ###")
