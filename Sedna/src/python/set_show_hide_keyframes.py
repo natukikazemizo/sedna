1e3adb9df317f0cb80fd0ec2ec47bf412ceffb5b
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-06-26 21:05:59 +0900

    Passive Rigid Body のキーフレームを削除
    Passive なRigid Bodyにキーフレームが不要だったので，キーフレームを削除した。

--- Sedna/src/python/set_show_hide_keyframes.py
+++ Sedna/src/python/set_show_hide_keyframes.py
@@ -19,18 +19,20 @@ targetParentName = "Cubes"
 bpy.data.scenes['Root.DorothyLoris'].frame_set(739)
 
 for child in bpy.data.objects[targetParentName].children:
-    child.hide = True
-    child.hide_render = True
-    child.keyframe_insert('hide', frame=739)
-    child.keyframe_insert('hide_render', frame=739)
+    if child.rigid_body.type == 'ACTIVE':
+        child.hide = True
+        child.hide_render = True
+        child.keyframe_insert('hide', frame=739)
+        child.keyframe_insert('hide_render', frame=739)
 
 bpy.data.scenes['Root.DorothyLoris'].frame_set(740)
 
 for child in bpy.data.objects[targetParentName].children:
-    child.hide = False
-    child.hide_render = False
-    child.keyframe_insert('hide', frame=740)
-    child.keyframe_insert('hide_render', frame=740)
+    if child.rigid_body.type == 'ACTIVE':
+        child.hide = False
+        child.hide_render = False
+        child.keyframe_insert('hide', frame=740)
+        child.keyframe_insert('hide_render', frame=740)
 
 print(PY_NAME + " END")
 
