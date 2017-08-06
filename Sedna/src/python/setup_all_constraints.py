492275f24cb9f03aef9fe56da90b2b9dcb5a7cbb
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-08-06 20:50:46 +0900

    顔のリグの

---
+++ Sedna/src/python/setup_all_constraints.py
@@ -0,0 +1,12 @@
+#!BPY
+# -*- coding: UTF-8 -*-
+# Set up All Constraints of Seleted Armature's Bones
+#
+# 2017.04.23 Natukikazemizo
+
+import utils_log
+import bpy
+import re
+
+# constants
+PY_NAME = "RENDER ALL"
