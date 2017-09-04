88e3fb8f3f2a869246d51cafd72602ecca4a7605
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-09-04 20:59:21 +0900

    ファイル入出力の.py作成開始

--- Sedna/src/python/utils/utils_io_file.py
+++ Sedna/src/python/utils/utils_io_file.py
@@ -5,6 +5,21 @@
 #
 #
 # 2017.08.29 Natukikazemizo
+import bpy
+import codecs
+
+
+f_in = codecs.open(bpy.path.abspath("//") + "data/in.txt", 'r', 'utf-8')
+f_out = codecs.open(bpy.path.abspath("//") + "data/out.txt", 'w', 'utf-8')
+
+data1 = f_in.read()
+f_in.close()
+lines1 = data1.split('n')
+for line in lines1:
+    print(line)
+    f_out.write(line)
+f_out.close()
+
 
 
 
