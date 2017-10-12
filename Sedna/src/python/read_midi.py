7643256791ed91248ed587aa4d5ab085d84114f0
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-10-12 22:12:04 +0900

    midiファイルからのデータの読み込み機能を実装途中
    read_midy.pyを動かすためには，
    pretty_midiのインストールが必要です。

--- Sedna/src/python/read_midi.py
+++ Sedna/src/python/read_midi.py
@@ -10,6 +10,7 @@ import os
 import utils_log
 import utils_io_csv
 import pretty_midi
+import math
 
 # init logger
 global logger
@@ -17,6 +18,17 @@ logger = utils_log.Util_Log(os.path.basename(__file__))
 
 logger.start()
 
+midi_data = pretty_midi.PrettyMIDI(bpy.path.abspath("//") + "data/MIDI/yamato_piano_rendan.mid")
+
+# GET 1ST TEMPO
+tempo = math.floor(midi_data.get_tempo_changes()[1][0])
+
+print("tempo:" + str(tempo))
+
+for index in range(4):
+    print(midi_data.instruments[index])
+    for note in midi_data.instruments[index].notes:
+        print(note)
 
 logger.end()
 
