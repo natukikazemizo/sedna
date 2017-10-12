852c2644b46ca8fdcf9d216a63161de549a08469
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-10-12 20:16:41 +0900

    midiファイルの読み込み処理の実装着手

---
+++ Sedna/src/python/read_midi.py
@@ -0,0 +1,22 @@
+#!BPY
+# -*- coding: UTF-8 -*-
+# Read MIDI File
+#
+# 2017.10.12 Natukikazemizo
+#
+
+import bpy
+import os
+import utils_log
+import utils_io_csv
+import pretty_midi
+
+# init logger
+global logger
+logger = utils_log.Util_Log(os.path.basename(__file__))
+
+logger.start()
+
+
+logger.end()
+
