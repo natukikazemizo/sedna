d075ea06399e7084e09bd62e03ebded5332d1a42
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2017-10-24 21:17:18 +0900

    リグのコントローラの修正
    ・唇のリグの修正
    ・肩のリグを修正

--- Sedna/src/python/pose_constraints_imp.py
+++ Sedna/src/python/pose_constraints_imp.py
@@ -10,7 +10,7 @@ import utils_log
 import utils_io_csv
 
 # Constants
-WORK_FILE_NAME = "pose_constraints_edit.csv"
+WORK_FILE_NAME = "pose_constraints_dl.csv"
 
 BONE_NAME = 0 
 CONSTRAINT_NAME = 1
@@ -39,65 +39,75 @@ OWNER_SPACE = 23
 
 # init logger
 global logger
+
+def imp(target):
+    header, data = utils_io_csv.read(WORK_FILE_NAME)
+
+    for row in data:
+        if bpy.data.objects.find(target) == -1:
+            logger.warn("Object not found. Object name is " + target)
+            break
+        if bpy.data.objects[target].pose.bones.find(row[BONE_NAME]) == -1:
+            logger.warn("Bone not found. Bone name is " + row[BONE_NAME])
+            break
+        bone = bpy.data.objects[target].pose.bones[row[BONE_NAME]]
+        
+        if bone.constraints.find(row[CONSTRAINT_NAME]) == -1:
+            constraint = bone.constraints.new(type="TRANSFORM")
+            constraint.name = row[CONSTRAINT_NAME]
+        
+        constraint = bone.constraints[row[CONSTRAINT_NAME]]
+        
+        print(bone.name + constraint.name)
+        
+        constraint.mute = row[MUTE] == "True"
+        constraint.target = bpy.data.objects[target]
+        constraint.subtarget = row[SUBTARGET_BONE_NAME]
+        constraint.use_motion_extrapolate = row[EXTRAPOLATE] == "True"
+        
+        constraint.from_min_x = float(row[FROM_MIN_X])
+        constraint.from_max_x = float(row[FROM_MAX_X])
+        constraint.from_min_y = float(row[FROM_MIN_Y])
+        constraint.from_max_y = float(row[FROM_MAX_Y])
+        constraint.from_min_z = float(row[FROM_MIN_Z])
+        constraint.from_max_z = float(row[FROM_MAX_Z])
+        
+        constraint.map_to_x_from = row[MAP_TO_X_FROM]
+        constraint.map_to_y_from = row[MAP_TO_Y_FROM]
+        constraint.map_to_z_from = row[MAP_TO_Z_FROM]
+        constraint.map_to = row[MAP_TO]
+        if constraint.map_to == "LOCATION":
+            constraint.to_min_x = float(row[TO_MIN_X])
+            constraint.to_max_x = float(row[TO_MAX_X])
+            constraint.to_min_y = float(row[TO_MIN_Y])
+            constraint.to_max_y = float(row[TO_MAX_Y])
+            constraint.to_min_z = float(row[TO_MIN_Z])
+            constraint.to_max_z = float(row[TO_MAX_Z])
+        elif constraint.map_to == "ROTATION":
+            constraint.to_min_x_rot = float(row[TO_MIN_X])
+            constraint.to_max_x_rot = float(row[TO_MAX_X])
+            constraint.to_min_y_rot = float(row[TO_MIN_Y])
+            constraint.to_max_y_rot = float(row[TO_MAX_Y])
+            constraint.to_min_z_rot = float(row[TO_MIN_Z])
+            constraint.to_max_z_rot = float(row[TO_MAX_Z])
+        else:
+            # map_to:SCALE
+            constraint.to_min_x_scale = float(row[TO_MIN_X])
+            constraint.to_max_x_scale = float(row[TO_MAX_X])
+            constraint.to_min_y_scale = float(row[TO_MIN_Y])
+            constraint.to_max_y_scale = float(row[TO_MAX_Y])
+            constraint.to_min_z_scale = float(row[TO_MIN_Z])
+            constraint.to_max_z_scale = float(row[TO_MAX_Z])
+
+        constraint.target_space = row[TARGET_SPACE]
+        constraint.owner_space = row[OWNER_SPACE]
+
 logger = utils_log.Util_Log(os.path.basename(__file__))
 
 logger.start()
 
-header, data = utils_io_csv.read(WORK_FILE_NAME)
-
-for row in data:
-    if bpy.context.object.pose.bones.find(row[BONE_NAME]) == -1:
-        logger.warn("Bone not found. Bone name is " + row[BONE_NAME])
-        break
-    if bpy.data.objects.find(row[TARGET]) == -1:
-        logger.warn("Object not found. Object name is " + row[TARGET])
-        break
-    bone = bpy.context.object.pose.bones[row[BONE_NAME]]
-    constraint = bone.constraints[row[CONSTRAINT_NAME]]
-    
-    print(bone.name + constraint.name)
-    
-    constraint.mute = row[MUTE] == "True"
-    constraint.target = bpy.data.objects[row[TARGET]]
-    constraint.subtarget = row[SUBTARGET_BONE_NAME]
-    constraint.use_motion_extrapolate = row[EXTRAPOLATE] == "True"
-    
-    constraint.from_min_x = float(row[FROM_MIN_X])
-    constraint.from_max_x = float(row[FROM_MAX_X])
-    constraint.from_min_y = float(row[FROM_MIN_Y])
-    constraint.from_max_y = float(row[FROM_MAX_Y])
-    constraint.from_min_z = float(row[FROM_MIN_Z])
-    constraint.from_max_z = float(row[FROM_MAX_Z])
-    
-    constraint.map_to_x_from = row[MAP_TO_X_FROM]
-    constraint.map_to_y_from = row[MAP_TO_Y_FROM]
-    constraint.map_to_z_from = row[MAP_TO_Z_FROM]
-    constraint.map_to = row[MAP_TO]
-    if constraint.map_to == "LOCATION":
-        constraint.to_min_x = float(row[TO_MIN_X])
-        constraint.to_max_x = float(row[TO_MAX_X])
-        constraint.to_min_y = float(row[TO_MIN_Y])
-        constraint.to_max_y = float(row[TO_MAX_Y])
-        constraint.to_min_z = float(row[TO_MIN_Z])
-        constraint.to_max_z = float(row[TO_MAX_Z])
-    elif constraint.map_to == "ROTATION":
-        constraint.to_min_x_rot = float(row[TO_MIN_X])
-        constraint.to_max_x_rot = float(row[TO_MAX_X])
-        constraint.to_min_y_rot = float(row[TO_MIN_Y])
-        constraint.to_max_y_rot = float(row[TO_MAX_Y])
-        constraint.to_min_z_rot = float(row[TO_MIN_Z])
-        constraint.to_max_z_rot = float(row[TO_MAX_Z])
-    else:
-        # map_to:SCALE
-        constraint.to_min_x_scale = float(row[TO_MIN_X])
-        constraint.to_max_x_scale = float(row[TO_MAX_X])
-        constraint.to_min_y_scale = float(row[TO_MIN_Y])
-        constraint.to_max_y_scale = float(row[TO_MAX_Y])
-        constraint.to_min_z_scale = float(row[TO_MIN_Z])
-        constraint.to_max_z_scale = float(row[TO_MAX_Z])
-
-    constraint.target_space = row[TARGET_SPACE]
-    constraint.owner_space = row[OWNER_SPACE]
+imp("Dorothy.Armature")
+imp("Loris.Armature")
 
 logger.end()
 
